8e74f1c391f1c6cc6b31c16e0f81a59fdd294a08
Alex Earl <slide.o.mix@gmail.com>
2024-11-27 20:51:22 -0700

    Clean-up freestyle-project
    Cleans up the freestyle-project plugin code based on recent Jenkins versions.

--- src/main/java/hudson/model/FreeStyleProject.java
+++ src/main/java/hudson/model/FreeStyleProject.java
@@ -1,18 +1,18 @@
 /*
  * The MIT License
- * 
+ *
  * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi, id:cactusman
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,22 +23,31 @@
  */
 package hudson.model;
 
+import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.Extension;
 import jenkins.model.Jenkins;
+import jenkins.model.item_category.StandaloneProjectsCategory;
 import org.jenkins.ui.icon.Icon;
 import org.jenkins.ui.icon.IconSet;
 import org.jenkinsci.Symbol;
-import jenkins.model.item_category.StandaloneProjectsCategory;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
 
 /**
  * Free-style software project.
- * 
+ *
  * @author Kohsuke Kawaguchi
  */
-public class FreeStyleProject extends Project<FreeStyleProject,FreeStyleBuild> implements TopLevelItem {
+public class FreeStyleProject extends Project<FreeStyleProject, FreeStyleBuild> implements TopLevelItem {
+
+    /**
+     * @deprecated as of 1.390
+     */
+    @Deprecated
+    public FreeStyleProject(Jenkins parent, String name) {
+        super(parent, name);
+    }
 
     public FreeStyleProject(ItemGroup parent, String name) {
         super(parent, name);
@@ -49,8 +58,9 @@ public class FreeStyleProject extends Project<FreeStyleProject,FreeStyleBuild> i
         return FreeStyleBuild.class;
     }
 
+    @Override
     public DescriptorImpl getDescriptor() {
-        return (DescriptorImpl)Jenkins.getInstance().getDescriptorOrDie(getClass());
+        return (DescriptorImpl) Jenkins.get().getDescriptorOrDie(getClass());
     }
 
     /**
@@ -60,22 +70,30 @@ public class FreeStyleProject extends Project<FreeStyleProject,FreeStyleBuild> i
      * @deprecated as of 2.0
      *      Use injection
      */
+    @Deprecated
     @Restricted(NoExternalUse.class)
+    @SuppressFBWarnings(value = "MS_PKGPROTECT", justification = "for backward compatibility")
     public static /*almost final*/ DescriptorImpl DESCRIPTOR;
 
-    @Extension(ordinal=1000) @Symbol({"freeStyle","freeStyleJob"})
+    @Extension(ordinal = 1000)
+    @Symbol({"freeStyle", "freeStyleJob"})
     public static class DescriptorImpl extends AbstractProjectDescriptor {
-        @SuppressFBWarnings("ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD")
+        @SuppressFBWarnings(
+                value = "ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD",
+                justification = "for backward compatibility")
         public DescriptorImpl() {
             DESCRIPTOR = this;
         }
 
+        @NonNull
+        @Override
         public String getDisplayName() {
             return LocalizedMessages.FreeStyleProject_DisplayName();
         }
 
+        @Override
         public FreeStyleProject newInstance(ItemGroup parent, String name) {
-            return new FreeStyleProject(parent,name);
+            return new FreeStyleProject(parent, name);
         }
 
         @Override
@@ -95,16 +113,19 @@ public class FreeStyleProject extends Project<FreeStyleProject,FreeStyleBuild> i
 
         @Override
         public String getIconClassName() {
-            return "icon-freestyle-project";
+            return "symbol-freestyle-project";
         }
 
         static {
-            IconSet.icons.addIcon(new Icon("icon-freestyle-project icon-sm", "16x16/freestyleproject.png", Icon.ICON_SMALL_STYLE));
-            IconSet.icons.addIcon(new Icon("icon-freestyle-project icon-md", "24x24/freestyleproject.png", Icon.ICON_MEDIUM_STYLE));
-            IconSet.icons.addIcon(new Icon("icon-freestyle-project icon-lg", "32x32/freestyleproject.png", Icon.ICON_LARGE_STYLE));
-            IconSet.icons.addIcon(new Icon("icon-freestyle-project icon-xlg", "48x48/freestyleproject.png", Icon.ICON_XLARGE_STYLE));
-            Items.XSTREAM.alias("project",FreeStyleProject.class);
+            IconSet.icons.addIcon(
+                    new Icon("icon-freestyle-project icon-sm", "16x16/freestyleproject.png", Icon.ICON_SMALL_STYLE));
+            IconSet.icons.addIcon(
+                    new Icon("icon-freestyle-project icon-md", "24x24/freestyleproject.png", Icon.ICON_MEDIUM_STYLE));
+            IconSet.icons.addIcon(
+                    new Icon("icon-freestyle-project icon-lg", "32x32/freestyleproject.png", Icon.ICON_LARGE_STYLE));
+            IconSet.icons.addIcon(
+                    new Icon("icon-freestyle-project icon-xlg", "48x48/freestyleproject.png", Icon.ICON_XLARGE_STYLE));
+            Items.XSTREAM.alias("project", FreeStyleProject.class);
         }
     }
-
 }
